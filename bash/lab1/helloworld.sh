624d493bc0b879eb15a23681d7c7c89789e8317f
brnarcher88 <brian.archer@mygeorgian.ca>
2016-09-26 08:37:32 -0400

    added helloworldalt.sh

---
+++ bash/lab1/helloworld.sh
@@ -0,0 +1,4 @@
+#!bin/bash
+#helloworld.sh ~ echoes “Hello World”
+echo “Hello world”
+
---
+++ bash/lab1/helloworldalt.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+# My second script 
+cat <<EOF 
+Hello World! 
+EOF
+
+#"Using a HERE document for data"
